5a0c9b93bdd8bc2f5b95ffc5db7a9c2a567ac309
woonie <woonie@gmail.com>
2017-03-14 02:51:36 +0900

    initial commit: improved variable names

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# karuta_anki
+# Karuta Flashcards
+I originally wrote this simple web app back in 2013. I had just joined Waseda University for a year-long exchange. Having watched Chihayafuru earlier in the year, I joined the university's karuta club and soon found memorizing the 100 *kimari-ji*s was no mean feat. 
+
+At that time, I was also just starting to learn web programming, so I decided to learn Javascript from scratch and create this flashcard app to help me with memorizing the *kimari-ji* for the game.
+
+*TODO: instructions*
---
+++ karuta.js
@@ -0,0 +1,289 @@
+var answer = ["&#12354;&#12365;&#12398;",
+"&#12399;&#12427;&#12377;",
+"&#12354;&#12375;",
+"&#12383;&#12372;",
+"&#12362;&#12367;",
+"&#12363;&#12373;",
+"&#12354;&#12414;&#12398;",
+"&#12431;&#12364;&#12356;",
+"&#12399;&#12394;&#12398;",
+"&#12371;&#12428;",
+"&#12431;&#12383;&#12398;&#12399;&#12425;&#12420;",
+"&#12354;&#12414;&#12388;",
+"&#12388;&#12367;",
+"&#12415;&#12385;",
+"&#12365;&#12415;&#12364;&#12383;&#12417;&#12399;",
+"&#12383;&#12385;",
+"&#12385;&#12399;",
+"&#12377;",
+"&#12394;&#12395;&#12431;&#12364;",
+"&#12431;&#12403;",
+"&#12356;&#12414;&#12371;",
+"&#12405;",
+"&#12388;&#12365;",
+"&#12371;&#12398;",
+"&#12394;&#12395;&#12375;",
+"&#12362;&#12368;",
+"&#12415;&#12363;&#12398;",
+"&#12420;&#12414;&#12374;",
+"&#12371;&#12371;&#12429;&#12354;",
+"&#12354;&#12426;&#12354",
+"&#12354;&#12373;&#12412;&#12425;&#12369;&#12354;",
+"&#12420;&#12414;&#12364;",
+"&#12402;&#12373;",
+"&#12383;&#12428;",
+"&#12402;&#12392;&#12431;",
+"&#12394;&#12388;",
+"&#12375;&#12425;",
+"&#12431;&#12377;&#12425;",
+"&#12354;&#12373;&#12376;",
+"&#12375;&#12398;",
+"&#12371;&#12356;",
+"&#12385;&#12366;&#12426;&#12365;",
+"&#12354;&#12356;",
+"&#12362;&#12362;&#12371;",
+"&#12354;&#12431;&#12428;",
+"&#12422;&#12425;",
+"&#12420;&#12360;",
+"&#12363;&#12380;&#12434;",
+"&#12415;&#12363;&#12365;",
+"&#12365;&#12415;&#12364;&#12383;&#12417;&#12362;",
+"&#12363;&#12367;",
+"&#12354;&#12369;",
+"&#12394;&#12370;&#12365;",
+"&#12431;&#12377;&#12428;",
+"&#12383;&#12365;",
+"&#12354;&#12425;&#12374;",
+"&#12417;",
+"&#12354;&#12426;&#12414;",
+"&#12420;&#12377;",
+"&#12362;&#12362;&#12360;",
+"&#12356;&#12395;",
+"&#12424;&#12434;",
+"&#12356;&#12414;&#12431;",
+"&#12354;&#12373;&#12412;&#12425;&#12369;&#12358;",
+"&#12358;&#12425;",
+"&#12418;&#12429;",
+"&#12399;&#12427;&#12398;",
+"&#12371;&#12371;&#12429;&#12395;",
+"&#12354;&#12425;&#12375;",
+"&#12373;",
+"&#12422;&#12358;",
+"&#12362;&#12392;",
+"&#12383;&#12363;",
+"&#12358;&#12363;",
+"&#12385;&#12366;&#12426;&#12362;",
+"&#12431;&#12383;&#12398;&#12399;&#12425;&#12371;",
+"&#12379;",
+"&#12354;&#12431;&#12376;",
+"&#12354;&#12365;&#12363;",
+"&#12394;&#12364;&#12363;",
+"&#12411;",
+"&#12362;&#12418;",
+"&#12424;&#12398;&#12394;&#12363;&#12424;",
+"&#12394;&#12364;&#12425;",
+"&#12424;&#12418;",
+"&#12394;&#12370;&#12369;",
+"&#12416;",
+"&#12394;&#12395;&#12431;&#12360;",
+"&#12383;&#12414;",
+"&#12415;&#12379;",
+"&#12365;&#12426;",
+"&#12431;&#12364;&#12381;",
+"&#12424;&#12398;&#12394;&#12363;&#12431;",
+"&#12415;&#12424;",
+"&#12362;&#12362;&#12369;",
+"&#12399;&#12394;&#12373;",
+"&#12371;&#12396;",
+"&#12363;&#12380;&#12381;",
+"&#12402;&#12392;&#12418;",
+"&#12418;&#12418;"]
+/*var answer = ["あきの","はるす","あし","たご","おく","かさ","あまの","わがい","はなの","これ",
+"わたのはらや","あまつ","つく","みち","きみがためは","たち","ちは","す","なにわが","わび",
+"いまこ","ふ","つき","この","なにし","おぐ","みかの","やまざ","こころあ","ありあ",
+"あさぼらけあ","やまが","ひさ","たれ","ひとは","なつ","しら","わすら","あさじ","しの",
+"こい","ちぎりき","あい","おおこ","あわれ","ゆら","やえ","かぜを","みかき","きみがためお",
+"かく","あけ","なげき","わすれ","たき","あらざ","め","ありま","やす","おおえ",
+"いに","よを","いまは","あさぼらけう","うら","もろ","はるの","こころに","あらし","さ",
+"ゆう","おと","たか","うか","ちぎりお","わたのはらこ","せ","あわじ","あきか","ながか",
+"ほ","おも","よのなかよ","ながら","よも","なげけ","む","なにわえ","たま","みせ",
+"きり","わがそ","よのなかは","みよ","おおけ","はなさ","こぬ","かぜそ","ひとも","もも"];
+*/
+var kimarijiSortedArray = [[87,57,22,81,70,18,77], //one
+[40,37,23,13,100,66,74,65,71,46], //two
+[61,21,63], //i
+[75,42,17], //chi
+[33,99,35], //hi
+[50,15,91], //ki
+[2,9,67,96], //ha
+[28,32,47,59], //ya
+[6,48,51,98], //ka
+[62,83,85,93], //yo
+[14,27,49,90,94], //mi
+[4,16,34,55,73,89], //ta
+[10,24,29,68,41,97], //ko
+[11,20,8,38,54,76,92], //wa
+[5,26,44,60,72,82,95], //o
+[19,25,36,53,80,84,86,88],//na
+[1,3,7,12,30,31,39,43,45,52,56,58,64,69,78,79]//a
+]
+function printArray(myArray){
+	var output = "(";	
+	for(var i = 0; i < myArray.length; i++){
+		var foo = myArray[i];
+		if (i > 0){
+			output = output + ", ";
+		}
+		output = output + answer[parseInt(foo)-1];
+	}
+	output = output + ")";
+	console.log(output);
+	return output
+}
+function revealAnswer(){
+	var temp = document.getElementById('cardValue').innerHTML;
+	temp = temp.substring(temp.indexOf("#")+1);
+	document.getElementById('ans').innerHTML = answer[parseInt(temp)-1];
+}
+function convertstring(num){
+	var v = num.toString();
+	if (v.length == 1){
+    		v = "00" + v;
+    	} else if (v.length == 2){
+    		v = "0" + v;
+    	} 
+    return v;
+}
+function createList(myArray){
+	var foo = []
+	for (var i = 0; i < myArray.length; i++){
+		var v = convertstring(myArray[i]);
+		foo.push(v);
+	}
+	return foo;
+}
+function range(start, end) {
+    var foo = [];
+    for (var i = start; i <= end; i++) {
+    	var v = convertstring(i);
+        foo.push(v);
+    }
+    return foo;
+}
+var bookmark = [];
+var historyArray = [];
+var remainingCards = range(1,100);
+var count = 0;
+var histCount = 0;
+var show = false;
+function resetDisplay(count, value){
+	document.getElementById('fuda').src = "";
+	document.getElementById('fuda').src ="./images/"+value+".png";
+	document.getElementById('questionNumber').innerHTML = count + ". ";
+	document.getElementById('cardValue').innerHTML = "#" + value;
+	document.getElementById('ans').innerHTML = "";
+}
+function endOfTestTimeout (){
+	document.getElementById('endOfQuiz').innerHTML = "";
+}
+function test(){
+	if (histCount == 0){
+		if (remainingCards.length > 0){
+			count++;
+			var randomIndex = Math.floor(Math.random() * remainingCards.length);
+			var value = remainingCards.splice( randomIndex, 1 );
+			historyArray.push(value);
+			resetDisplay(count, value);
+		} else{
+			document.getElementById('endOfQuiz').innerHTML = " end of card list";
+			setTimeout(endOfTestTimeout, 500);
+		}
+	} else{ // user has used back button
+		histCount++;
+		var index = count + histCount;
+		value = historyArray[count+histCount-1];
+		resetDisplay(index, value);
+	}
+}
+function reset(){
+	remainingCards = chooseQuestion();
+	count = 0;
+	histCount = 0;
+	historyArray = [];
+	test();
+	
+}
+function back(){
+	histCount--;
+	var index = count + histCount;
+	if (index > 0){
+		value = historyArray[count+histCount-1];
+		resetDisplay(index, value);
+	} else{
+		histCount++;
+	}
+}
+var selection = ["one", "two","i","chi","hi","ki","ha","ya","ka","yo", "mi","ta","ko","wa","o","na","a"];
+function chooseQuestion(){
+	if (document.getElementById("all").checked){
+		console.log("all true");
+		return range(1,100);
+	}
+	if (document.getElementById("bookmark").checked){
+		console.log("bookmark");
+		return createList(bookmark);
+	}
+	var foo = [];
+	for (var i = 0; i < selection.length; i++){
+		if (document.getElementById(selection[i]).checked){
+			var temp1 = createList(kimarijiSortedArray[i]);
+			foo = foo.concat(temp1);
+		}
+
+	}
+	if (foo.length == 0){
+		return range(1,100);
+	} else{
+		return foo;
+	}
+}
+function addCard(){
+	var temp = document.getElementById('cardValue').innerHTML;
+	temp = temp.substring(temp.indexOf("#")+1);
+	if (temp == ""){
+		return
+	}
+	var idx = bookmark.indexOf(temp);
+	console.log("idx: "+idx);
+	if (idx == -1){
+		bookmark.push(temp);
+		if (show){
+			document.getElementById('customList').innerHTML = printArray(bookmark);
+		}
+	}
+	console.log(bookmark);
+}
+function removeCard(){
+	var temp = document.getElementById('cardValue').innerHTML;
+	temp = temp.substring(temp.indexOf("#")+1);
+	var idx = bookmark.indexOf(temp);
+	if (idx > -1){
+		bookmark.splice(idx, 1);
+		if (show){
+			document.getElementById('customList').innerHTML = printArray(bookmark);
+		}
+	}
+	console.log(bookmark);
+}
+function showSaved(){
+	if (!show){
+		document.getElementById('customList').innerHTML = printArray(bookmark);
+		document.getElementById('showlist').value = "hide";
+		show = true
+
+	} else{
+		document.getElementById('customList').innerHTML = "";
+		document.getElementById('showlist').value = "show";
+		show = false
+	}
+}
